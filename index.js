98b027a732fc695fe2b18877c2d979de56f145f0
Anu0104 <anushkavisapure@gmail.com>
2024-12-11 23:39:05 +0530

    chore: add nodejs api helm chart

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Product = require("./models/product.model.js");
 const productRoute = require("./routes/product.route.js");
 const app = express();
+require('dotenv').config();
 
 // middleware
 app.use(express.json());
@@ -15,10 +16,10 @@ app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
 
+const dbURI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
+
 mongoose
-  .connect(
-    'mongodb://admin:adminpassword@mongodb-container:27017/testdb?authSource=admin'
-  )
+  .connect(dbURI)
   .then(() => {
     console.log("Connected to database!");
     app.listen(3000, () => {
--- terraform-nodejs-api-infra/providers.tf
+++ terraform-nodejs-api-infra/providers.tf
@@ -18,11 +18,11 @@ terraform {
 }
 
 data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "default" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
